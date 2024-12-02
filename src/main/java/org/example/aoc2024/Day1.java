44d0e32ecc690754f3c32876197f8e4ed225b142
maciu <cristihubati03@gmail.com>
2024-12-02 22:40:51 +0200

    first commit 2024

---
+++ src/main/java/org/example/aoc2024/Day1.java
@@ -0,0 +1,16 @@
+package org.example.aoc2024;
+
+import org.example.utils.Day;
+
+public class Day1 implements Day {
+
+    @Override
+    public void Part1() {
+
+    }
+
+    @Override
+    public void Part2() {
+
+    }
+}
