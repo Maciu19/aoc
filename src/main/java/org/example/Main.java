ab5c89d392722773ae440a6b581f5da2b3bf1989
maciu <cristihubati03@gmail.com>
2024-12-03 23:56:42 +0200

    Added Day 2 Part 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.aoc2024.day2.Day2;
+
 public class Main {
     public static void main(String[] args) {
+        Day2 d2 = new Day2();
 
+        d2.Part2();
     }
 }
--- src/main/java/org/example/aoc2024/day2/Day2.java
+++ src/main/java/org/example/aoc2024/day2/Day2.java
@@ -2,15 +2,73 @@ package org.example.aoc2024.day2;
 
 import org.example.utils.Day;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day2 implements Day {
 
     @Override
     public void Part1() {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(Day.INPUT_FILE_PATH + "/day2/day2.txt"));
+            List<List<Integer>> reports = new ArrayList<>();
 
+            for(String line : lines) {
+                String[] numbers = line.split(" ");
+                List<Integer> levels = new ArrayList<>();
+
+                for(String numberString: numbers) {
+                    int number = Integer.parseInt(numberString);
+                    levels.add(number);
+                }
+
+                reports.add(levels);
+            }
+
+            int safeCounter = 0;
+            for(List<Integer> report : reports) {
+                if(isSafe(report)) {
+                    safeCounter++;
+                }
+            }
+
+            System.out.println(safeCounter);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void Part2() {
 
+    }
+
+    private boolean isSafe(List<Integer> arr) {
+        boolean increasingOrder = true;
+
+        for (int i = 0; i < arr.size() - 1; i++) {
+            int subtract = arr.get(i) - arr.get(i + 1);
+            int difference = Math.abs(subtract);
+
+            if(!(difference >= 1 && difference <= 3))
+                return false;
+
+            if (i == 0 && subtract < 0) {
+                increasingOrder = false;
+            }
+
+            if (subtract > 0 && !increasingOrder)
+                return false;
+
+            if (subtract < 0 && increasingOrder)
+                return false;
+        }
+
+        return true;
     }
 }
